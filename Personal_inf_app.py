cfaae0a86eee74fd5d53763b2870700a90868e41
akhror <151764079+akhroralishev@users.noreply.github.com>
2025-02-11 20:01:57 +0500

    Create Personal_inf_app.py
    Personal information app

---
+++ Personal_inf_app.py
@@ -0,0 +1,13 @@
+ """**Personal Information App**
+   - Create variables to store your name, age, height (in meters), and whether you're a student
+   - Print all this information in a formatted message"""
+# O'zgaruvchilarni yaratamiz
+name = "Akhror"
+age = 21
+height = 1.74
+is_student = True  # Talabalik holati (True - ha, False - yo'q)
+
+# Formatlangan xabar,
+print(f"Ismim - {name}, Yoshim - {age}, Bo'yim - {height}, Talabaman - {is_student}")
+
+
